1b70e2784ff869086f6f4d5c48ce58aee9e75201
profilike <vasylpb89@gmail.com>
2017-06-03 16:26:34 +0300

    first commit

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
+import { DevTools } from './utils/index';
 
 function _applyMiddleware() {
     const middleware = [];
@@ -8,7 +9,8 @@ function _applyMiddleware() {
 
 export default function configureStore(initialState) {
     const store = compose(
-        _applyMiddleware()
+        _applyMiddleware(),
+        DevTools.instrument()
     )(createStore)(rootReducer, initialState);
     return store;
 }
